ccfde62055e4890b82c3e6d32eed11b52a9548eb
Mathys <mathyswl10@gmail.com>
2026-03-18 22:17:41 +0100

    Fix chemin script + debug affichage erreur backend

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-// Met en MAJUSCULE automatiquement
-function formatTag() {
-    let input = document.getElementById("tag");
-    input.value = input.value.toUpperCase();
-}
-
-// Appel backend
 async function getPlayer() {
     let tag = document.getElementById("tag").value;
-
-    if (!tag) {
-        document.getElementById("result").innerText = "Entre un tag";
-        return;
-    }
 
     try {
         const res = await fetch(`http://localhost:3000/player/${tag}`);
         const data = await res.json();
+
+        console.log(data); // 🔥 debug
 
         if (data.name) {
             document.getElementById("result").innerText =
                 "Pseudo : " + data.name;
         } else {
             document.getElementById("result").innerText =
-                "Erreur : joueur introuvable";
+                "Erreur API";
         }
 
     } catch (err) {
         document.getElementById("result").innerText =
-            "Erreur serveur";
+            "Backend non accessible";
     }
 }
